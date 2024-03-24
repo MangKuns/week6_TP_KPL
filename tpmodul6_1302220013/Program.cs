8cea5250c8040182d52d3b863fba323be2895582
Rizky Kusuma Nugraha <kusumarizky65@gmail.com>
2024-03-24 08:51:07 +0700

    mplementasi design by contract

--- tpmodul6_1302220013/Program.cs
+++ tpmodul6_1302220013/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
 class SayaTubeVideo
 {
@@ -6,8 +6,14 @@ class SayaTubeVideo
     private string title;
     private int playCount;
 
-    public SayaTubeVideo(String title)
+    public SayaTubeVideo(string title)
     {
+ 
+        if (string.IsNullOrEmpty(title) || title.Length > 100)
+        {
+            throw new ArgumentException("Judul video harus memiliki panjang maksimal 100 karakter dan tidak null.");
+        }
+
         id = GenerateRandomId();
         this.title = title;
         playCount = 0;
@@ -15,12 +21,28 @@ class SayaTubeVideo
 
     private int GenerateRandomId()
     {
-        Random randomID = new Random();
-        return randomID.Next(10000, 99999);
+        Random random = new Random();
+        return random.Next(10000, 99999); 
     }
-    public void increasePLayCount(int count)
+
+    public void IncreasePlayCount(int count)
     {
-        playCount =+ count;
+        if (count < 0 || count > 10000000)
+        {
+            throw new ArgumentOutOfRangeException("count", "Input penambahan play count harus antara 0 hingga 10,000,000.");
+        }
+
+        try
+        {
+            checked
+            {
+                playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Overflow terjadi dalam penambahan play count.");
+        }
     }
 
     public void PrintVideoDetails()
@@ -35,16 +57,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-        string namaPraktikan = "Rizky Kusuma Nugraha";
-        string judulVideo = $"Tutorial Design By Contract - {namaPraktikan}";
+        try
+        {
+           
+            SayaTubeVideo video = new SayaTubeVideo(null); 
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"ArgumentException: {ex.Message}");
+        }
 
-        SayaTubeVideo video = new SayaTubeVideo(judulVideo);
+        try
+        {
+            
+            SayaTubeVideo video = new SayaTubeVideo("Video dengan judul panjang melebihi 100 karakter ini akan menyebabkan pengecualian ArgumentException.");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"ArgumentException: {ex.Message}");
+        }
 
-        video.PrintVideoDetails();
+        try
+        {
+            
+            SayaTubeVideo video = new SayaTubeVideo("Video 1");
 
-        video.increasePLayCount(5);
+            video.IncreasePlayCount(20000000); 
 
-        video.PrintVideoDetails();
+            for (int i = 0; i < 5; i++)
+            {
+                video.IncreasePlayCount(2000000); 
+            }
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine($"ArgumentOutOfRangeException: {ex.Message}");
+        }
+
+        SayaTubeVideo video1 = new SayaTubeVideo("Video 1");
+        video1.PrintVideoDetails();  
+        video1.IncreasePlayCount(5); 
+        video1.PrintVideoDetails();  
     }
 }
--- tpmodul6_1302220013/obj/Debug/net8.0/tpmodul6_1302220013.AssemblyInfo.cs
+++ tpmodul6_1302220013/obj/Debug/net8.0/tpmodul6_1302220013.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul6_1302220013")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e938e2bc919b5c513b3ae11a256fe892bf25a786")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6faf7d9172eb079cdb4c7ecf03953252016d302a")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul6_1302220013")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul6_1302220013")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
